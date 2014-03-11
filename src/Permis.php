74f0a9079485caaee9e3354c99842daef921c845
erickthrash <ecabestanym@gmail.com>
2014-03-11 17:18:41 +0100

    esborro carpeta ajuda amb la meva configuracio de Windows i Doctrine

--- src/Permis.php
+++ src/Permis.php
@@ -32,5 +32,100 @@ class Permis
     private $usuaris;
    
     
+    /**
+     * Constructor
+     */
+    public function __construct()
+    {
+        $this->usuaris = new \Doctrine\Common\Collections\ArrayCollection();
+    }
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set nom
+     *
+     * @param string $nom
+     * @return Permis
+     */
+    public function setNom($nom)
+    {
+        $this->nom = $nom;
+
+        return $this;
+    }
+
+    /**
+     * Get nom
+     *
+     * @return string 
+     */
+    public function getNom()
+    {
+        return $this->nom;
+    }
+
+    /**
+     * Set descripcio
+     *
+     * @param string $descripcio
+     * @return Permis
+     */
+    public function setDescripcio($descripcio)
+    {
+        $this->descripcio = $descripcio;
+
+        return $this;
+    }
+
+    /**
+     * Get descripcio
+     *
+     * @return string 
+     */
+    public function getDescripcio()
+    {
+        return $this->descripcio;
+    }
+
+    /**
+     * Add usuaris
+     *
+     * @param \Usuari $usuaris
+     * @return Permis
+     */
+    public function addUsuari(\Usuari $usuaris)
+    {
+        $this->usuaris[] = $usuaris;
+
+        return $this;
+    }
+
+    /**
+     * Remove usuaris
+     *
+     * @param \Usuari $usuaris
+     */
+    public function removeUsuari(\Usuari $usuaris)
+    {
+        $this->usuaris->removeElement($usuaris);
+    }
+
+    /**
+     * Get usuaris
+     *
+     * @return \Doctrine\Common\Collections\Collection 
+     */
+    public function getUsuaris()
+    {
+        return $this->usuaris;
+    }
 }
-?>
--- src/Usuari.php
+++ src/Usuari.php
@@ -38,6 +38,146 @@ class Usuari
     private $permisos;
    
     
-}
+    /**
+     * Constructor
+     */
+    public function __construct()
+    {
+        $this->permisos = new \Doctrine\Common\Collections\ArrayCollection();
+    }
 
-?>
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set nom
+     *
+     * @param string $nom
+     * @return Usuari
+     */
+    public function setNom($nom)
+    {
+        $this->nom = $nom;
+
+        return $this;
+    }
+
+    /**
+     * Get nom
+     *
+     * @return string 
+     */
+    public function getNom()
+    {
+        return $this->nom;
+    }
+
+    /**
+     * Set contrasenya
+     *
+     * @param string $contrasenya
+     * @return Usuari
+     */
+    public function setContrasenya($contrasenya)
+    {
+        $this->contrasenya = $contrasenya;
+
+        return $this;
+    }
+
+    /**
+     * Get contrasenya
+     *
+     * @return string 
+     */
+    public function getContrasenya()
+    {
+        return $this->contrasenya;
+    }
+
+    /**
+     * Set UltimaVisita
+     *
+     * @param \DateTime $ultimaVisita
+     * @return Usuari
+     */
+    public function setUltimaVisita($ultimaVisita)
+    {
+        $this->UltimaVisita = $ultimaVisita;
+
+        return $this;
+    }
+
+    /**
+     * Get UltimaVisita
+     *
+     * @return \DateTime 
+     */
+    public function getUltimaVisita()
+    {
+        return $this->UltimaVisita;
+    }
+
+    /**
+     * Set email
+     *
+     * @param string $email
+     * @return Usuari
+     */
+    public function setEmail($email)
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+    /**
+     * Get email
+     *
+     * @return string 
+     */
+    public function getEmail()
+    {
+        return $this->email;
+    }
+
+    /**
+     * Add permisos
+     *
+     * @param \Permis $permisos
+     * @return Usuari
+     */
+    public function addPermiso(\Permis $permisos)
+    {
+        $this->permisos[] = $permisos;
+
+        return $this;
+    }
+
+    /**
+     * Remove permisos
+     *
+     * @param \Permis $permisos
+     */
+    public function removePermiso(\Permis $permisos)
+    {
+        $this->permisos->removeElement($permisos);
+    }
+
+    /**
+     * Get permisos
+     *
+     * @return \Doctrine\Common\Collections\Collection 
+     */
+    public function getPermisos()
+    {
+        return $this->permisos;
+    }
+}
