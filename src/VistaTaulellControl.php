74d56196ec00317271442f445b96a985c8ffb9f0
jordi1292 <jordi1292@gmail.com>
2014-04-07 19:32:16 +0200

    funcio pintaErrors

--- src/VistaTaulellControl.php
+++ src/VistaTaulellControl.php
@@ -6,14 +6,47 @@
 class VistaTaulellControl
 {
 	public function getUltimsError()
-    {
+	{
 
-    	
 
-        $query = $entityManager->createQuery('SELECT r FROM Registre r WHERE r.sensor.tipusSensor = 0 ORDER BY r.id DESC')->setMaxResults(10);
-		$tipusSensor = $query->getResult();
-		return $tipusSensor;
-    }
+
+		$query = $entityManager->createQuery('SELECT r FROM Registre r WHERE r.sensor.tipusSensor = 0 ORDER BY r.id DESC')->setMaxResults(10);
+		$ultimsErrors = $query->getResult();
+		return $ultimsErrors;
+	}
+
+
+	public function pintaErrors()
+	{	
+		?>
+		<div class="panel panel-default">
+			<div class="panel-heading">
+				<i class="fa fa-bell fa-fw"></i> Panell de Notificacions
+			</div>
+			<!-- /.panel-heading -->
+			<div class="panel-body">
+				<div class="list-group">
+					<?php
+					$arrayRegistres = getUltimsError();
+					foreach ($arrayRegistres as $key => $registre) {						
+					
+					echo '<a href="#" class="list-group-item list-group-item-danger">
+						<i class="fa fa-warning fa-fw"></i>'. $registre->valor.'
+						<span class="pull-right text-muted small"><em>'.$registre->dataHora.'</em>
+						</span>
+					</a>';
+					}
+					?>
+				</div>
+				<a href="Error.html" class="btn btn-default btn-block">Veure tots els errors</a>
+			</div>
+			<!-- /.panel-body -->
+		</div>
+		<!-- /.panel -->
+	<?php
+
+	}
+
 
 
 }
