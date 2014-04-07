f7c6e95d8ad5d7ae2ca28f01072ba05f823091c4
jordi1292 <jordi1292@gmail.com>
2014-04-07 18:43:09 +0200

    creacio vistaTaulellControl amb la funcio getUltimsErrors

--- src/VistaTaulellControl.php
+++ src/VistaTaulellControl.php
@@ -8,11 +8,11 @@ class VistaTaulellControl
 	public function getUltimsError()
     {
 
-    	Query::setMaxResults(10);
+    	
 
-        $query = $entityManager->createQuery('SELECT r FROM Registre r WHERE r.sensor.tipusSensor = 0');
+        $query = $entityManager->createQuery('SELECT r FROM Registre r WHERE r.sensor.tipusSensor = 0 ORDER BY r.id DESC')->setMaxResults(10);
 		$tipusSensor = $query->getResult();
-
+		return $tipusSensor;
     }
 
 
